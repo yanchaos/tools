8e596a4f66bba77f40983ccafb936e4590422998
Chaos <yanchaos@aliyun.com>
2019-08-13 16:46:48 +0800

    新增浏览器类型判断

--- JavaScript/common.tools.js
+++ JavaScript/common.tools.js
@@ -114,3 +114,14 @@ window.chaos = {};
 	}
 
 }(window.chaos, jQuery));
+(function($){
+	//判断浏览器类型
+	var userAgent = navigator.userAgent.toLowerCase();
+	jQuery.browser = {
+		version: (userAgent.match(/.+(?:rv|it|ra|ie)[\/: ](\d.+)/) || [])[1],
+		safari: /webkit/.test(userAgent),
+		opera: /opera/.test(userAgent),
+		msie: /msie/.test(userAgent) && !/opera/.test(userAgent),
+		mozilla: /mozilla/.test(userAgent) && !/(compatible|webkit)/.test(userAgent)
+	};
+}(jQuery));
